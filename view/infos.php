48cc8f758957081ae30012115aa541d9c1394850
JonathanDLR <marie89>
2019-12-22 20:24:44 +0100

    google plan

--- view/infos.php
+++ view/infos.php
@@ -3,14 +3,14 @@ ob_start();
 ?>
     <section>
 
-        <?php if ($_SESSION['type'] !== "civil") { ?>
+        <?php if (($_SESSION['type'] == "both") || ($_SESSION['type'] == "laique")) { ?>
             <article id="prieure">
                 <h2>Informations Prieuré</h2>
                 <div>
                     <div><img src="web/img/prieure.jpg" alt="prieure" /></div>
 
                     <p>Nous vous donnons rendez-vous le samedi 19 septembre 2020 à partir de 14H au Prieuré de
-                     Saint-Ellier à Charcé-Saint-Ellier-sur-Aubance, 49320 Brissac-Loire-Aubance.</p>
+                     Saint-Ellier<br/> à Charcé-Saint-Ellier-sur-Aubance, 49320 Brissac-Loire-Aubance.</p>
 
                     <p>Pour venir plusieurs moyens s'offrent à vous:
                         <ul>	
@@ -23,7 +23,7 @@ ob_start();
                     </p>
 
                     <div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d411018.88176925457!2d-0.3714271192563251!3d47.40823745583819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4807dc0b257a2895%3A0x40d37521e0d97d0!2sCharc%C3%A9-Saint-Ellier-sur-Aubance%2C%2049320%20Brissac-Loire-Aubance!5e0!3m2!1sfr!2sfr!4v1576871364546!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>                    </div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d411018.88176925457!2d-0.3714271192563251!3d47.40823745583819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4807dc0b257a2895%3A0x40d37521e0d97d0!2sCharc%C3%A9-Saint-Ellier-sur-Aubance%2C%2049320%20Brissac-Loire-Aubance!5e0!3m2!1sfr!2sfr!4v1576871364546!5m2!1sfr!2sfr" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                     <div>
 
                 <div>
@@ -31,10 +31,23 @@ ob_start();
                 </div>
             </article>
         <?php } ?>
+        <?php if (($_SESSION['type'] == "both") || ($_SESSION['type'] == "civil")) { ?>
+            <article id="mairie">
+                <h2>Informations Mairie</h2>
+                <div>
+                    <p>Vous êtes conviés le samedi 22 août 2020 à partir de 10h30 à la mairie de Cruas <br/>
+                    Place René Cassin, 07350 Cruas.</p>
 
-        <article id="mairie">
-            <h2>Informations Mairie</h2>
-        <article>
+                    <div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d90824.51505412822!2d4.71365361723687!3d44.65291480155687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b540e09afbdfcd%3A0xd35ee713f7f5287d!2sPlace%20Ren%C3%A9%20Cassin%2C%2007350%20Cruas!5e0!3m2!1sfr!2sfr!4v1577037564229!5m2!1sfr!2sfr" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                        </div>
+                </div>
+
+                <div>
+                    <h3>Programme de la Journée</h3>
+                </div>
+            <article>
+        <?php } ?>
     </section>
 <?php $contain = ob_get_clean();
 require($_SERVER['DOCUMENT_ROOT'].'/mariage/view/common/template.php'); ?>
